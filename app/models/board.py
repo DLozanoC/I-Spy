ffd36bf7798533de59cd35862cdc5aef1df248de
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-12-22 15:34:17 -0800

    initial setup complete

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Board(db.Model):
+    __tablename__= 'board'
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,13 @@
 from app import db
+
+class Card(db.Model):
+    __tablename__= 'card'
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+
+
+# Card, table name: card
+# card_id, int, primary key
+# message, string
+# likes_count, int
